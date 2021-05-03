47be217c9b16fb33fe3f887c118795a186125415
JScotty99 <jaronlscotland@gmail.com>
2021-05-03 13:16:30 -0400

    test

--- mail.php
+++ mail.php
@@ -6,5 +6,5 @@ $recipient = "jaronlscotland@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";;
+echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
